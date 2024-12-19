58862070b29533798ecfbec8578700326a596577
PROEO6EEXP <ekkejakob@gmail.com>
2024-12-20 01:23:37 +0200

    Update Program.cs

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -4,5 +4,12 @@
 //rakendus küsib kasutajal sisestada tema nime
 //rakendus tervitab kasutajat nimepidi
 
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName = Console.ReadLine();
 
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !");
 
